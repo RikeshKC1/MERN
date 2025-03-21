3b80987825a69af6d912a833c29ed173580d9a60
RikeshKC1 <pranishkecy102@gmail.com>
2025-03-21 21:14:57 +0545

    pushing files

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const express = require("express");
 
 const app = express();
 
+PORT = 5000
+
+
+app.listen(5000, () => {
+             console.log(`Server running on port ${PORT}`)
+});  
+
+app.use('/',(req,res)=>{
+    console.log("Hello World!");
+
+});
